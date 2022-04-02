774906f2fde0e638bba1a03a22e15f4e5cee709e
fpiskur <filip.piskur@gmail.com>
2022-04-02 12:20:01 +0200

    Add followers association

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,6 +91,7 @@ class UserTest < ActiveSupport::TestCase
     refute michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     refute michael.following?(archer)
     # Users can't follow themselves
