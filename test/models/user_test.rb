229a9661f6d6688c3fcd3ffab90ec76efe074913
fpiskur <filip.piskur@gmail.com>
2022-04-03 16:28:24 +0200

    Write a test for status feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -98,4 +98,26 @@ class UserTest < ActiveSupport::TestCase
     michael.follow(michael)
     refute michael.following?(michael)
   end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Self-posts for user with followers
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Self-posts for user with no followers
+    archer.microposts.each do |post_self|
+      assert archer.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      refute michael.feed.include?(post_unfollowed)
+    end
+  end
 end
